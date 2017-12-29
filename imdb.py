bf12c0fdd4c8aeef39e78a8a0158f4ff56caa4e9
Hallicopter <advait.raykar@gmail.com>
2017-12-29 22:32:43 +0530

    Can function on a page of search results

--- imdb.py
+++ imdb.py
@@ -9,12 +9,12 @@ class ImdbSpider(scrapy.Spider):
 
     def parse(self, response):
         self.log('Just visited '+ response.url)
-        films_src = response.xpath("//div[1]/div[1]/div[3]/div/div[3]").extract_first()
-        
+        films_src = response.xpath("//div[1]/div[1]/div[3]/div/div[3]")
         for film in films_src:
-            film{
-                'film_name':films_src.xpath("h3[1]/a[1]/text()").extract(),
-                'director': films_src.xpath("p[3]/a/text()").extract()[0],
-                'lead_actors': films_src.xpath("p[3]/a/text()").extract()[1:],
-                'imdb_link': 'http://www.imdb.com'+films_src.xpath("h3[1]/a[1]/@href").extract()[0]
+            film_dat = {
+                'film_name' :film.xpath("h3[1]/a[1]/text()").extract(),
+                'director'  : film.xpath("p[3]/a/text()").extract()[0],
+                'lead_actors': film.xpath("p[3]/a/text()").extract()[1:],
+                'imdb_link' : 'http://www.imdb.com'+film.xpath("h3[1]/a[1]/@href").extract()[0]
             }
+            print(film_dat)
