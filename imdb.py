85e54566e6474635f6c3d299702efa85645db678
Hallicopter <advait.raykar@gmail.com>
2017-12-29 23:00:03 +0530

    Adds support to scrape multiple pages

--- imdb.py
+++ imdb.py
@@ -5,7 +5,8 @@ import scrapy
 class ImdbSpider(scrapy.Spider):
     name = 'imdb'
     allowed_domains = ['imdb.com']
-    start_urls = ['http://www.imdb.com/search/title?countries=in&languages=hi&title_type=feature']
+    src = 'http://www.imdb.com/search/title?countries=%C2%B7%C2%B7%C2%B7%C2%A0Common%20Countries%C2%A0%C2%B7%C2%B7%C2%B7&groups=top_100&languages=%C2%B7%C2%B7%C2%B7%C2%A0Common%20Languages%C2%A0%C2%B7%C2%B7%C2%B7&title_type=feature'
+    start_urls = [src]
 
     def parse(self, response):
         self.log('Just visited '+ response.url)
@@ -17,4 +18,9 @@ class ImdbSpider(scrapy.Spider):
                 'lead_actors': film.xpath("p[3]/a/text()").extract()[1:],
                 'imdb_link' : 'http://www.imdb.com'+film.xpath("h3[1]/a[1]/@href").extract()[0]
             }
-            print(film_dat)
+            yield film_dat
+        # Follow the pagination link
+        next_page_url = response.xpath("//div[1]/div[1]/div[4]/div[1]/a[1]/@href").extract()[0]
+        if next_page_url:
+            next_page_url = response.urljoin(next_page_url)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
